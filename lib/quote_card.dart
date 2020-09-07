385edbab28b5be23a02f5af89d2225e2669baeca
raj patel <kakhilesh79@gmail.com>
2020-09-07 11:26:46 +0530

    final setup

---
+++ lib/quote_card.dart
@@ -0,0 +1,49 @@
+import 'package:flutter/material.dart';
+import 'quote.dart';
+
+class QuoteCard extends StatelessWidget {
+  final Quote quote;
+  final Function delete;
+  QuoteCard({this.quote, this.delete});
+
+  @override
+  Widget build(BuildContext context) {
+    return Card(
+      margin: EdgeInsets.fromLTRB(16, 16, 16, 5),
+      child: Padding(
+        padding: const EdgeInsets.all(12.0),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.stretch,
+          children: [
+            Text(
+              quote.text,
+              style: TextStyle(
+                fontSize: 18,
+                color: Colors.grey[600],
+              ),
+            ),
+            SizedBox(
+              height: 6,
+            ),
+            Text(
+              quote.author,
+              style: TextStyle(
+                fontSize: 14,
+                color: Colors.grey[800],
+              ),
+            ),
+            SizedBox(
+              height: 8,
+            ),
+            FlatButton.icon(
+              onPressed:delete,
+              icon: Icon(Icons.delete),
+              label: Text('Delete'),
+              
+            )
+          ],
+        ),
+      ),
+    );
+  }
+}
